817477e111f0aeae86f5f680d8764ac04008e75c
Pop Cristian-Gabriel <57450421+PopCristianGabriel@users.noreply.github.com>
2020-06-17 20:03:50 +0300

    Add files via upload

---
+++ php/lab6 php/database.php
@@ -0,0 +1,10 @@
+<?php
+
+$servername = "DESKTOP-97SDQ88";
+$username = "root"
+$password = ""
+$dbname = "Ajax"
+
+$conn = mysqli_connect($servername,$username,$password,$dbname);
+
+?>
---
+++ php/lab6 php/get_users_by_role.php
@@ -0,0 +1,31 @@
+<?php
+
+
+
+Welcome <?php echo $_POST["fname"]; ?>.<br />​
+
+You are <?php echo $_POST["age"]; ?> years old! 
+
+ 
+
+
+	/*include 'database.php';
+	$role = $_POST["role"];
+		$sql = "Select * from AjaxUser where role ="+$role;
+		$result = mysqli_query($conn,$sql);
+		if(mysqli_num_rows($result) > 0){
+			while($row = mysqli_fetch_assoc($result)){
+				echo "<p>";
+				echo $row;
+				echo "<br>"
+				echo "</p>"
+			}
+		}
+		else{
+			echo "There are no results";
+		}*/
+?>
+
+
+
+	
